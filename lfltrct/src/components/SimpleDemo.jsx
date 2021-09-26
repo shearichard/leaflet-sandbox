238168b6969129ff8e0c295db1cc1f06c5f40bae
Richard Shea <rshea@thecubagroup.com>
2021-09-27 11:51:32 +1300

    Baseline for the react app

---
+++ lfltrct/src/components/SimpleDemo.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+class App extends React.Component {
+    render(){
+        return (
+			<main>
+                <div id="mapid">
+                    <h2>Placeholder Text</h2>
+                </div>
+            </main>
+        )
+    }
+}
+
+export default App
