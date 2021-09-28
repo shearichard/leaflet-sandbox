dac099cb3802206fdff6ac9cb462271973edf0f3
Richard Shea <rshea@thecubagroup.com>
2021-09-28 17:05:47 +1300

    Tweak look and feel

--- lfltrct/src/App.jsx
+++ lfltrct/src/App.jsx
@@ -8,6 +8,11 @@ class App extends React.Component {
 
     render(){
         return (<div className="mycontent">
+
+                    <>
+                    <h1>react-leaflet Sandbox Simple Demo</h1>
+                    <p>This is a simple example using the React Leaflet project.</p>
+                    </>
                     <SimpleDemo id="mapid"/>
                 </div>                
         )
