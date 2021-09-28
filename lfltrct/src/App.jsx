0e80a1df1e5de39c39e6bb36e62d1ca8e596c46e
Richard Shea <rshea@thecubagroup.com>
2021-09-28 16:53:53 +1300

    First map rendering using leaflet-react

--- lfltrct/src/App.jsx
+++ lfltrct/src/App.jsx
@@ -8,7 +8,7 @@ class App extends React.Component {
 
     render(){
         return (<div className="mycontent">
-                    <SimpleDemo />
+                    <SimpleDemo id="mapid"/>
                 </div>                
         )
     }
--- lfltrct/src/components/SimpleDemo.jsx
+++ lfltrct/src/components/SimpleDemo.jsx
@@ -1,13 +1,24 @@
 import React from "react"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import 'leaflet/dist/leaflet.css'
 
 class App extends React.Component {
     render(){
-        return (
-			<main>
-                <div id="mapid">
-                    <h2>Placeholder Text</h2>
-                </div>
-            </main>
+        const position = [-36.8484, 174.7621]
+                
+        return(
+            <MapContainer style={{ height: "450px", width: "100vh" }} center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
         )
     }
 }
