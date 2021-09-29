048ad43d4131eeb383bab30547165512d4d359d3
Richard Shea <rshea@thecubagroup.com>
2021-09-29 15:37:50 +1300

    Make overlay square follow a grid of streets and reflect which streets in comment above map

--- lfltrct/src/components/SimpleDemo.jsx
+++ lfltrct/src/components/SimpleDemo.jsx
@@ -6,15 +6,15 @@ import 'leaflet/dist/leaflet.css'
 class App extends React.Component {
     render(){
         const position = [-36.8484, 174.7621]
-        const latlngs = [[-36.8503325,174.7633283],[-36.84740269087975, 174.76575297151567],[-36.84596244369074, 174.76083916477606],[-36.849401767006874, 174.75931164657456]]
+        const latlngs = [[-36.85088433890884, 174.76450574430237],[-36.84740269087975, 174.76575297151567],[-36.84596244369074, 174.76083916477606],[-36.849401767006874, 174.75931164657456]]
         
         const purpleOptions = { color: 'purple' }
         const popuptext = "The Auckland Sky Tower."
                 
         return(
-            <MapContainer id="mapid" style={{ height: "450px", width: "600px" }} center={position} zoom={16} scrollWheelZoom={false}>
+            <MapContainer id="mapid" style={{ height: "450px", width: "600px" }} center={position} zoom={15} scrollWheelZoom={false}>
                 <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    attribution= 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
                 <Polygon pathOptions={purpleOptions} positions={latlngs}/>
